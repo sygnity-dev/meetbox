e86e96257d39c5e4c7dc836d6834660d7c0abc59
Dariusz Depta <dariusz.depta@engos.software>
2021-05-14 10:56:08 +0200

    Fixed local audio.

--- src/gui.js
+++ src/gui.js
@@ -65,15 +65,15 @@ export const gui = (function () {
    *
    */
   const init = (configuration) => {
-    this.localVideo = window.document.getElementById('mbxLocalVideo');
-    if (this.localVideo && configuration && configuration.localVideo) {
+    localVideo = window.document.getElementById('mbxLocalVideo');
+    if (localVideo && configuration && configuration.localVideo) {
       if (configuration.localVideo.width) {
-        this.localVideo.style.width = configuration.localVideo.width;
+        localVideo.style.width = configuration.localVideo.width;
       }
     }
     hideLocalVideo();
     hideRemoteVideo();
-    this.localVideo.addEventListener('playing', (event) => {
+    localVideo.addEventListener('playing', (event) => {
       logger.info('playing LOCAL video: width=' + event.target.videoWidth + ' height=' + event.target.videoHeight);
     });
     remoteVideo().addEventListener('playing', (event) => {
@@ -156,7 +156,7 @@ export const gui = (function () {
    * @return {HTMLElement}
    */
   const getLocalVideo = function () {
-    return localVideo;
+    return window.document.getElementById('mbxLocalVideo');
   }
 
   /**
@@ -220,7 +220,7 @@ export const gui = (function () {
     showButton('mbxMicOnButton', true);
     showButton('mbxMicOffButton', false);
     pipe.micOn();
-    this.localVideo.muted = true;
+    localVideo.muted = true;
   }
 
   /**
@@ -230,7 +230,7 @@ export const gui = (function () {
     showButton('mbxMicOnButton', false);
     showButton('mbxMicOffButton', true);
     pipe.micOff();
-    this.localVideo.muted = true;
+    localVideo.muted = true;
   }
 
   /**
